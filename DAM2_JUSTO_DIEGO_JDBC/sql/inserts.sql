5ed390ecb6765d9c95a5d2a51438f9b17a7f39ff
diegojg2 <diejusgar@gmail.com>
2026-05-28 09:26:51 +0200

    primer push 9:25

---
+++ DAM2_JUSTO_DIEGO_JDBC/sql/inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO AGENCIA VALUES (1, 'Gato', 'Francia', TO_DATE('2000-05-10', 'YYYY-MM-DD'), 'Diego Justo');
+INSERT INTO AGENCIA VALUES (1, 'Loro', 'Italia', TO_DATE('2000-05-10', 'YYYY-MM-DD'), 'Diego Justo');
+INSERT INTO AGENCIA VALUES (1, 'Cacatua', 'España', TO_DATE('2000-05-10', 'YYYY-MM-DD'), 'Diego Justo');
+INSERT INTO AGENCIA VALUES (1, 'Perro', 'Francia', TO_DATE('2000-05-10', 'YYYY-MM-DD'), 'Diego Justo');
+INSERT INTO AGENCIA VALUES (1, 'Leon', 'España', TO_DATE('2000-05-10', 'YYYY-MM-DD'), 'Diego Justo');
+
+
+INSERT INTO SATELITE VALUES (1, 'ALTA', 500000, 10000000, 'ACTIVO', TO_DATE('2000-05-10', 'YYYY-MM-DD'), 1, 'Diego Justo');
---
+++ DAM2_JUSTO_DIEGO_JDBC/src/examen/justo/diego/motores/PostgreMotorSQL.java
@@ -0,0 +1,4 @@
+package examen.justo.diego.motores;
+
+public class PostgreMotorSQL {
+}
